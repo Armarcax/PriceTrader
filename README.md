ea03176fba9258fff3e55f58aad162fa14cb68d8
Armarcax <arcaxa305@gmail.com>
2026-03-22 02:04:30 +0400

    Initial MVP version

---
+++ README.md
@@ -0,0 +1,1 @@
+# PriceTrader MVP
---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>PriceTrader</h1>
+      <p>Frontend initialized</p>
+    </div>
+  );
+}
